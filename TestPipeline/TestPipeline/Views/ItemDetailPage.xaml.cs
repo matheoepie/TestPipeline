86014dc6f9ef78ee961ee41cd975e80337c7f01d
matheoepie <matheoepie@outlook.com>
2021-04-08 22:16:06 +0200

    Ajoutez des fichiers projet.

---
+++ TestPipeline/TestPipeline/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TestPipeline.ViewModels;
+using Xamarin.Forms;
+
+namespace TestPipeline.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ TestPipeline/TestPipeline/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using TestPipeline.Models;
+using TestPipeline.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestPipeline.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
